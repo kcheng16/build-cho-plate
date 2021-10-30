812382319bc25c84db9be8e6eea8b4df59f3aae0
kenny <kcheng16@gmail.com>
2021-10-29 21:49:06 -0700

    added button to clear-food and eat-plate, added eventListener for eat-food to +=1 currentLevel

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import level from "./scripts/level";
 
 document.addEventListener("DOMContentLoaded",() => {
 
-console.log("Hello World");
+// console.log("Hello World");
 const main = document.getElementById("main");
 new Example(main);
 
 //========================================================
 const foodBank = document.getElementById("food-bank");
-var currentLevel = 1
+var currentLevel = 1;
 
   level[currentLevel].food.forEach(itemName => {
     let li = document.createElement("li");
@@ -19,11 +19,17 @@ var currentLevel = 1
     li.innerText = itemName;
     foodBank.appendChild(li);
 
-    console.log(img);
     if (Object.keys(food).includes(itemName)){
       img.src = food[itemName].img;
-      console.log(food[itemName].img);
       li.appendChild(img);
     }
   })
+
+  //Eat food===================
+  const eatButton = document.getElementById("eat-food")
+  eatButton.addEventListener("click", function(){ 
+    currentLevel += 1
+  });
+  
+  //
 });
