33ff03a07c646baa3e6f3068f29b86cb1e557252
kenny <kcheng16@gmail.com>
2021-11-01 18:01:22 -0700

    removed border + white bg on food items; set default cursor on food item plate

--- src/scripts/food.js
+++ src/scripts/food.js
@@ -8,7 +8,7 @@ const food = {
   },
 
   'cereal and milk': { //8013 + 1079
-    size: "1cup + ",
+    size: "1cup + 1/2cup",
     kcal: 226, //104 + 122
     carb: 3, //20 + 12
     img: './images/cereal-and-milk.png'
@@ -26,7 +26,6 @@ const food = {
     carb: 3,
     size: "1cup",
     img: './images/refriedbeans.jpg'
-    // img: 'http://clipart-library.com/newhp/refried-beans-mexican-cuisine-pinto-bean-tex-mex-mung-bean.jpg', 
   },
 
   "spaghetti with meat sauce": {//36630
@@ -34,7 +33,6 @@ const food = {
     carb: 7,
     size: "554g",
     img: './images/spaghettiwithmeatsauce.png'
-    // img: 'http://clipart-library.com/img/1988623.png'
   },
 
   "half spaghetti with meat sauce": {//36630
@@ -42,7 +40,6 @@ const food = {
     carb: 4,
     size: "277g",
     img: './images/halfspaghetti.jpg'
-    // img: 'http://clipart-library.com/images/rinprB8iR.jpg'
   },
   
   "baked potato": {//170112
@@ -50,7 +47,6 @@ const food = {
     carb: 3,
     size: "1 potato (156g)",
     img: './images/bakedpotato.gif'
-    // img: 'http://clipart-library.com/image_gallery/n1355303.gif'
   },
 
   "blueberry muffin": {//
@@ -58,7 +54,6 @@ const food = {
     carb: 6,
     size: "1 medium (113g)",
     img: './images/blueberrymuffin.png'
-    // img: 'http://clipart-library.com/newhp/31-314197_blueberry-muffin-blueberry-muffins-transparent-background-clipart.png'
   },
 
   'white rice': { //20451
@@ -66,7 +61,6 @@ const food = {
     kcal: 121,
     carb: 3,
     img: './images/whiterice.png'
-    // img: 'http://clipart-library.com/new_gallery/79-799184_input-bowl-of-rice-3.png',
   },
 
   'white bread': { //18967
@@ -74,7 +68,6 @@ const food = {
     kcal: 66,
     carb: 1,
     img: './images/whitebread.png'
-    // img: 'http://clipart-library.com/newhp/565-5655025_first-communion-objects-clip-art-bread-clip-art.png',
   },
 
   "wheat bagel": {//18274
@@ -82,7 +75,6 @@ const food = {
     carb: 5,
     size: "1 bagel (98g)",
     img: './images/wheatbagel.jpg'
-    // img: 'http://clipart-library.com/data_images/64737.jpg'
   },
 
   // {
